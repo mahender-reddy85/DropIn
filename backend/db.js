8f2745be3cbd0470ed025087d38b77f2507869b5
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2025-11-26 00:34:02 +0530

    .

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'likki@8585',
-  database: 'dropin'
+  database: 'dropin',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = db;
+module.exports = pool;
