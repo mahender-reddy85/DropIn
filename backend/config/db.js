f8d18300fd272f6a4349b6d5ee3afef0824b6f8a
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2026-03-19 13:56:21 +0530

    mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI && process.env.NODE_ENV === 'production') {
+      console.error('FATAL ERROR: MONGO_URI is not defined in the environment variables.');
+      process.exit(1);
+    }
     const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/dropin';
     const conn = await mongoose.connect(mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
